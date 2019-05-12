61c5fac0762a0968e477bc39d0ab66b195102a04
aniketrb-github <aniketbharsakale@gmail.com>
2019-05-12 22:16:48 +0530

    restructuring commit for double check locking in Singleton design pattern

--- design_patterns/src/main/java/singleton_design_pattern/DoubleCheckedLockInSingleton.java
+++ design_patterns/src/main/java/singleton_design_pattern/DoubleCheckedLockInSingleton.java
@@ -1,24 +1,24 @@
-package double_check_locking_concept;
+package singleton_design_pattern;
 
-public class Singleton {
+public class DoubleCheckedLockInSingleton {
 
 	// If we dont use volatile keyword, then other threads are able to see the half
 	// instantiated state of object. volatile keyword guarantees the happens-before relationship
 	// i.e All the write operation on 'object' will happen-before any read operation is done on it.
-	private volatile static Singleton object;
+	private volatile static DoubleCheckedLockInSingleton object;
 	
 	// Private Contructor prevents object instantiation of this class
-	private Singleton() {
+	private DoubleCheckedLockInSingleton() {
 	}
 	
 	// Here, we perform double check locking
-	public static Singleton getInstance() {
+	public static DoubleCheckedLockInSingleton getInstance() {
 		if (object == null) {
 			
 			// synchronized block, prevents race condition between multiple threads
-			synchronized (Singleton.class) {
+			synchronized (DoubleCheckedLockInSingleton.class) {
 				if (object == null) {
-					object = new Singleton();
+					object = new DoubleCheckedLockInSingleton();
 				}
 			}
 		}
